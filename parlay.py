56a2ff4578d469fccc3f7ee432894082ba556781
cesarglez133 <121037947@upq.edu.mx>
2023-03-06 16:37:08 -0600

    Se editaron los botones de la interfaz de parlay

--- parlay.py
+++ parlay.py
@@ -3,7 +3,7 @@ from tkinter import *
 #ventana
 ventana= Tk()   
 ventana.geometry("800x600")
-ventana.title("momio")
+ventana.title("Parlay")
 
 #frames
 
@@ -12,55 +12,55 @@ seccion1.pack(expand=True,fill='both')
 
 #texto
 
-txtinicio= Label(seccion1,text="cantidad de momios:")
+txtinicio= Label(seccion1,text="Cantidad de momios:")
 txtinicio.configure(font=("calibri", 13))
 txtinicio.place(x=270,y=30)
 
-txtCorreo= Label(seccion1,text="momio1:",fg="black")
+txtCorreo= Label(seccion1,text="Momio1:",fg="black")
 txtCorreo.configure(font=("calibri",13))
 txtCorreo.place(x=20,y=85)
 
-txtCorreo= Label(seccion1,text="momio2:",fg="black")
+txtCorreo= Label(seccion1,text="Momio2:",fg="black")
 txtCorreo.configure(font=("calibri",13))
 txtCorreo.place(x=20,y=165)
 
-txtContra= Label(seccion1,text="equpo 1",fg="black")
+txtContra= Label(seccion1,text="Equipo 1",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=115,y=80)
 
-txtContra= Label(seccion1,text="equpo 2",fg="black")
+txtContra= Label(seccion1,text="Equipo 2",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=405,y=80)
 
-txtContra= Label(seccion1,text="empate",fg="black")
+txtContra= Label(seccion1,text="Empate",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=260,y=80)
 
-txtContra= Label(seccion1,text="equpo 1",fg="black")
+txtContra= Label(seccion1,text="Equipo 1",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=115,y=160)
 
-txtContra= Label(seccion1,text="equpo 2",fg="black")
+txtContra= Label(seccion1,text="Equipo 2",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=405,y=160)
 
-txtContra= Label(seccion1,text="empate",fg="black")
+txtContra= Label(seccion1,text="Empate",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=260,y=160)
 
-txtContra= Label(seccion1,text="selescciones su prediccion:",fg="black")
+txtContra= Label(seccion1,text="Seleccione su predicción:",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=550,y=70)
 
-txtContra= Label(seccion1,text="selescciones su prediccion:",fg="black")
+txtContra= Label(seccion1,text="Seleccione su predicción:",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=550,y=150)
 
-txtContra= Label(seccion1,text="cantidad a apostar:",fg="black")
+txtContra= Label(seccion1,text="Cantidad a apostar:",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=130,y=220)
 
-txtContra= Label(seccion1,text="total a ganar:",fg="black")
+txtContra= Label(seccion1,text="Total a ganar:",fg="black")
 txtContra.configure(font=("calibri",13))
 txtContra.place(x=130,y=250)
 #entry
@@ -92,32 +92,32 @@ Contraseña= Entry(seccion1, bg="#F3F3F3")
 Contraseña.place(x=232, y=256)
 #los botones
 
-btonloG= Button(seccion1,text="momio",bg="white")
+btonloG= Button(seccion1,text="Momio",bg="white")
 btonloG.place(x=25,y=30)
 
-btonloG= Button(seccion1,text="individual",bg="white")
+btonloG= Button(seccion1,text="Individual",bg="white")
 btonloG.place(x=140,y=30)
 
-btonloG= Button(seccion1,text="equipo1",bg="white")
+btonloG= Button(seccion1,text="Equipo1",bg="white")
 btonloG.place(x=550,y=105)
 
-btonloG= Button(seccion1,text="empate",bg="white")
+btonloG= Button(seccion1,text="Empate",bg="white")
 btonloG.place(x=610,y=105)
 
-btonloG= Button(seccion1,text="equipo2",bg="white")
+btonloG= Button(seccion1,text="Equipo2",bg="white")
 btonloG.place(x=670,y=105)
 
-btonloG= Button(seccion1,text="equipo1",bg="white")
+btonloG= Button(seccion1,text="Equipo1",bg="white")
 btonloG.place(x=550,y=185)
 
-btonloG= Button(seccion1,text="empate",bg="white")
+btonloG= Button(seccion1,text="Empate",bg="white")
 btonloG.place(x=610,y=185)
 
-btonloG= Button(seccion1,text="equipo2",bg="white")
+btonloG= Button(seccion1,text="Equipo2",bg="white")
 btonloG.place(x=670,y=185)
 
 
-btonloG= Button(seccion1,text="guardar",bg="white")
+btonloG= Button(seccion1,text="Guardar",bg="white")
 btonloG.place(x=470,y=230)
 
 ventana.mainloop()
